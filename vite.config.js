ed22e78514da8c2de049117762ed3dd06d5ee618
張善程 <zhangshancheng@Stevens-Mac-mini.local>
2025-09-02 22:58:19 +0800

    Initial deploy

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// vite.config.js 加上
+export default defineConfig({
+  base: '/',  // 重要！
+  plugins: [react()]
+})
